acdb15eef81ab0bc53a61ce81e2dca719345b8cd
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-03-01 20:23:40 +0700

    adding avatarcards and images,json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
 import './App.css';
-import Container from './Container';
+import AvatarCards from './AvatarCards';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Container />
+      <AvatarCards />
     </div>
   );
 }
---
+++ src/AvatarCards.jsx
@@ -0,0 +1,15 @@
+import "./AvatarCards.css"
+import IMAGES from "./shared/images.json"
+
+function AvatarCards() {
+    return (
+        <div>
+            IMAGES && IMAGES.map((item) =>
+             
+            )
+        </div>
+        
+    )
+}
+
+export default AvatarCards;
